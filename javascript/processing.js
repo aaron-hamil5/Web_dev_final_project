6f855716abaed2518edceef5a57e4c35bced615d
Aaron <aaronleon28@gmail.com>
2025-12-15 16:25:56 +0000

    Now grabs data from the textarea

--- javascript/processing.js
+++ javascript/processing.js
@@ -1,6 +1,6 @@
 function processText() {
     //https://www.tutorialspoint.com/find-number-of-spaces-in-a-string-using-javascript
-    var str = "This is an example of spaces in   string";
+    var str = document.getElementById("paragraph").value;
     var numOfSpaces = str.split(" ").length - 1;
     document.getElementById("result").innerHTML = numOfSpaces;
 }
