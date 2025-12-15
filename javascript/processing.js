54791f9db105cfef0a536c2bdbbdd7bf17c6750f
Aaron <aaronleon28@gmail.com>
2025-12-15 16:52:42 +0000

    Scoured through w3schools to. build the logic

--- javascript/processing.js
+++ javascript/processing.js
@@ -1,6 +1,12 @@
 function processText() {
     //https://www.tutorialspoint.com/find-number-of-spaces-in-a-string-using-javascript
-    var str = document.getElementById("paragraph").value;
-    var numOfSpaces = str.split(" ").length - 1;
-    document.getElementById("result").innerHTML = numOfSpaces;
+    var inputtedText = document.getElementById("paragraph").value;
+    //https://www.w3schools.com/jsref/jsref_lastindexof.asp
+    var lastUwsIndex = inputtedText.lastIndexOf("uws");
+    //https://www.w3schools.com/jsref/jsref_substring.asp
+    var toRemove = inputtedText.substring(0, lastUwsIndex);
+    //https://www.w3schools.com/jsref/jsref_replace.asp
+    var updatedText = inputtedText.replace(toRemove, "");
+    var numOfSpaces = updatedText.split(" ").length - 1;
+    document.getElementById("result").innerHTML = "There is " + numOfSpaces + " spaces upto the last 'uws' in the paragraph.";
 }
